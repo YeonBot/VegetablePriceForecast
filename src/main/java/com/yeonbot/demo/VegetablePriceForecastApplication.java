9ebbc983dcbfa6a72520342c03e42977439e410a
tjdds1109 <tjdds1109@naver.com>
2019-04-08 12:31:47 +0900

    Initial commit

---
+++ src/main/java/com/yeonbot/demo/VegetablePriceForecastApplication.java
@@ -0,0 +1,13 @@
+package com.yeonbot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetablePriceForecastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VegetablePriceForecastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeonbot/demo/VegetablePriceForecastApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yeonbot.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VegetablePriceForecastApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
